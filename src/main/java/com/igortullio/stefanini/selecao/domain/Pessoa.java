753b1494835cf426d885d7ce7cada3ac8781f692
Igor Túllio <igor.goncalves@basis.com.br>
2019-03-19 09:59:39 -0300

    Adição do modelo lógico nosql e atualização da classe Pessoa

--- src/main/java/com/igortullio/stefanini/selecao/domain/Pessoa.java
+++ src/main/java/com/igortullio/stefanini/selecao/domain/Pessoa.java
@@ -2,21 +2,19 @@ package com.igortullio.stefanini.selecao.domain;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-@Document(collection = "pessoa")
+@Document
 public class Pessoa {
     @Id
     private String id;
     private String nome;
     private String cpf;
     private String email;
-    @DBRef(lazy = true)
     private List<Endereco> enderecos = new ArrayList<>();
 
     public Pessoa(String id, String nome, String cpf, String email) {
